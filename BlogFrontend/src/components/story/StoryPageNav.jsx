21cf7c0f0d3291c5ca36d0fc303c29666dc22150
Sanaullah Mobini <webdevfarsi@gmail.com>
2024-07-26 22:12:33 +0430

    post writing url

---
+++ BlogFrontend/src/components/story/StoryPageNav.jsx
@@ -0,0 +1,23 @@
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { BsThreeDots } from "react-icons/bs";
+
+
+const StoryPageNav = () => {
+  return (
+    <nav className="flex bg-blue-500 items-center justify-between py-3">
+        {/* logo */}
+        <div className="flex items-center gap-5">
+        <h1 className="font-bold tracking-wide text-3xl ">Medium</h1>
+        <div className="mt-2 font-light hidden lg:block">Draft in <span className="text-sm font-medium">Sanaulluh Mobini</span> </div>
+        </div>
+        <ul className="gap-5 items-center pt-2 hidden md:flex">
+            <li className="cursor-pointer bg-green-600 text-white px-2  rounded-3xl">publish</li>
+            <li className="cursor-pointer"><BsThreeDots size={24} /></li>
+            <li className="cursor-pointer"><IoIosNotificationsOutline size={24} /></li>
+            <li  className="w-8 rounded-full cursor-pointer bg-pink-400 h-8 border"></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default StoryPageNav
--- BlogFrontend/src/pages/NewStory.jsx
+++ BlogFrontend/src/pages/NewStory.jsx
@@ -1,8 +1,44 @@
+import StoryPageNav from "../components/story/StoryPageNav"
 
 const NewStory = () => {
+
+    const getTitle = (e) => {
+        if(e.keyCode == 13){
+            e.preventDefault();
+        }
+    }
+    const handleTitleField = (e) =>{
+        console.log(e);
+        let input = e.target;
+
+        input.style.height = 'auto';
+        input.style.height = input.scrollHeight + "px";
+    }
+
   return (
-    <div>NewStory</div>
+    <div className="max-w-[900px] w-full px-10 lg:px-0 mx-auto">
+        <StoryPageNav />
+        <div className="mt-3 relative w-full h-[320px] rounded-md overflow-hidden min-h-[320px] md:h-auto border border-gray-300">
+            <img className=" absolute top-0 left-0 w-full h-full object-cover opacity-55" src="https://cdn.pixabay.com/photo/2018/07/25/16/00/art-3561710_640.jpg" alt="banner" />
+            <label className="text-3xl capitalize absolute top-0 left-0 w-full h-full flex text-white justify-center items-center font-bold z-[1000] cursor-pointer" htmlFor="file">banner for Post</label>
+            <input 
+            className="hidden"
+            type="file"
+            id="file"
+            accept=".png .jpg"
+             />
+        </div>
+            <div className="w-full mt-5">
+                <textarea
+                onKeyDown={getTitle}
+                placeholder="Title"
+                onChange={handleTitleField}
+                className="placeholder:opacity-60 w-full break-words text-3xl  md:text-4xl lg:text-5xl outline-none h-20 resize-none"
+                ></textarea>
+            </div>
+    </div>
   )
+
 }
 
 export default NewStory
