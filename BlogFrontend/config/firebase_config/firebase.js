c8556fd72592ae169617e72f646f47a3c91507a9
Sanaullah Mobini <webdevfarsi@gmail.com>
2024-08-02 22:16:52 +0430

    Editor page

---
+++ BlogFrontend/config/firebase_config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLdy4p6Pt-R0EHxvcjsypnkK4hLVDhJ28",
+  authDomain: "medium-clone-26e92.firebaseapp.com",
+  projectId: "medium-clone-26e92",
+  storageBucket: "medium-clone-26e92.appspot.com",
+  messagingSenderId: "730100712539",
+  appId: "1:730100712539:web:1a4a3aecb8dcf6870f6b7e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- BlogFrontend/src/components/editorTools/EditorTools.jsx
+++ BlogFrontend/src/components/editorTools/EditorTools.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 // importing tools 
 
 import Embed from '@editorjs/embed';
@@ -7,22 +8,22 @@ import Header from '@editorjs/header'
 import Quote from '@editorjs/quote'
 import Marker from '@editorjs/marker'
 import inlineCode from '@editorjs/inline-code'
+import ImageTool from '@editorjs/image';
 
-
-const uploadImageByUrl = (e) => {
-    let link = new Promise((resolve,reject) => {
-        try {
-            resolve(e)
-        } catch (error) {
-            reject(error)
-        }
-    })
-    return link.then(url => {
-        return {
-            file: {url}
-        }
-    })
-}
+// const uploadImageByUrl = (e) => {
+//     let link = new Promise((resolve,reject) => {
+//         try {
+//             resolve(e)
+//         } catch (error) {
+//             reject(error)
+//         }
+//     })
+//     return link.then(url => {
+//         return {
+//             file: {url}
+//         }
+//     })
+// }
 
 // const uploadImageByFile = () => {
 
@@ -34,12 +35,57 @@ export const tools = {
         class: List,
         inlineToolbar: true
     },
+    // image: Image,
     image: {
         class: Image,
         config: {
-            uploader:{
-                uploadByUrl: uploadImageByUrl,
-            }
+            // uploader:{
+            //     uploadByUrl: ImageTool,
+            // }
+            // endpoints: {
+            //     byFile: 'http://localhost:8008/uploadFile', // Your backend file uploader endpoint
+            //     byUrl: 'http://localhost:8008/fetchUrl', // Your endpoint that provides uploading by Url
+            //   }
+            image: {
+                class: ImageTool,
+                config: {
+                  uploader: {
+                    async uploadByFile(file) {
+                      // your own uploading logic here
+                      const formData = new FormData();
+                      formData.append("file", file);
+      
+                      const response = await axios.post(
+                        `http://localhost:4001/api/uploadImage/create`,
+                        formData,
+                        {
+                          headers: {
+                            "Content-Type": "multipart/form-data",
+                          },
+                          withCredentials: false,
+                        }
+                      );
+      
+                      if (response.data.success === 1) {
+                        return response.data;
+                      }
+                    },
+                    // async uploadByUrl(url) {
+                    //   const response = await axios.post(
+                    //     `http://localhost:4001/api/uploadImage/createByUrl`,
+                    //     {
+                    //       url,
+                    //     }
+                    //   );
+      
+                    //   if (response.data.success === 1) {
+                    //     return response.data;
+                    //   }
+                    // },
+                  },
+                  inlineToolbar: true,
+                },
+              },
         }
     },
     header: {
